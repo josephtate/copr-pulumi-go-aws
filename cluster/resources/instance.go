005c91c16900206e3fe6f902a6d5053d80632ed9
Joseph S. Tate <jtate@ciq.com>
2024-08-08 15:15:01 -0400

    VPC and KeyPair changes * Refactor security group attachment * Set up and use KeyPairs from SSM Parameters

--- cluster/resources/instance.go
+++ cluster/resources/instance.go
@@ -2,43 +2,57 @@ package resources
 
 import (
 	"fmt"
+	"path/filepath"
+	"sync"
 
 	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/ec2"
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/ssm"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
 
-// Please note that "backend" is named such because it is the executing engine of COPR, not the due to the typical
-// frontend/backend development architecture. It is not the backend of the application.
-func CreateInstance(
-	ctx *pulumi.Context,
-	config *config.Config,
-	name string,
-	securityGroups map[string]*ec2.SecurityGroup,
-) (*ec2.Instance, error) {
-	resourcePrefix := config.Require("resourcePrefix")
-	instanceType := config.Require("instanceTypeBackend")
-	subnetID := getFirstSubnetID(config, true)
+var sshKeyCache sync.Map
 
-	amiID, err := GetLatestRocky9Ami(ctx)
-	if err != nil {
-		return nil, err
+// SetupSSHKey sets up the SSH key for the instances using Pulumi and memoizes the result.
+func SetupSSHKey(ctx *pulumi.Context, name, ssmPath string) (*ec2.KeyPair, error) {
+	if key, ok := sshKeyCache.Load(ssmPath); ok {
+		return key.(*ec2.KeyPair), nil
 	}
-	// Launch an EC2 instance with the resourcePrefix
-	inst, err := ec2.NewInstance(ctx, resourcePrefix+name, &ec2.InstanceArgs{
-		InstanceType:             pulumi.String(instanceType),
-		Ami:                      pulumi.String(amiID),
-		SubnetId:                 pulumi.String(subnetID),
-		AssociatePublicIpAddress: pulumi.Bool(true),
-		DisableApiTermination:    pulumi.Bool(config.Require("debug") != "true"),
+
+	// Will break if run from Windows
+	publicKeyParam := filepath.Join(ssmPath, "public-key")
+
+	publicKey, err := ssm.LookupParameter(ctx, &ssm.LookupParameterArgs{
+		Name: publicKeyParam,
 	})
 	if err != nil {
 		return nil, err
 	}
 
+	sshKey, err := ec2.NewKeyPair(ctx, name, &ec2.KeyPairArgs{
+		KeyNamePrefix: pulumi.String(name),
+		PublicKey:     pulumi.String(publicKey.Value),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	sshKeyCache.Store(ssmPath, sshKey)
+	return sshKey, nil
+}
+
+func AttachSecurityGroups(
+	ctx *pulumi.Context,
+	config *config.Config,
+	name string,
+	inst *ec2.Instance,
+	securityGroups map[string]*ec2.SecurityGroup,
+) error {
+	resourcePrefix := config.Require("resourcePrefix")
+
 	for n, sg := range securityGroups {
 		// Attach the security group to the instance
-		_, err = ec2.NewNetworkInterfaceSecurityGroupAttachment(
+		_, err := ec2.NewNetworkInterfaceSecurityGroupAttachment(
 			ctx,
 			fmt.Sprintf("%s%s-%s-sga", resourcePrefix, name, n),
 			&ec2.NetworkInterfaceSecurityGroupAttachmentArgs{
@@ -47,9 +61,54 @@ func CreateInstance(
 			},
 		)
 		if err != nil {
-			return nil, err
+			return err
 		}
 	}
+	return nil
+}
+
+// Please note that "backend" is named such because it is the executing engine of COPR, not the due to the typical
+// frontend/backend development architecture. It is not the backend of the application.
+func CreateInstance(
+	ctx *pulumi.Context,
+	config *config.Config,
+	name string,
+	securityGroups map[string]*ec2.SecurityGroup,
+	public bool,
+) (*ec2.Instance, error) {
+	resourcePrefix := config.Require("resourcePrefix")
+	sshKeyPath := config.Require("sshKeySSMPathBase")
+	instanceType := config.Require("instanceTypeBackend")
+	vpcProject := config.Require("vpcProjectName")
+
+	subnetID, err := GetFirstSubnet(ctx, vpcProject, public)
+	if err != nil {
+		return nil, err
+	}
+
+	amiID, err := GetLatestRocky9Ami(ctx)
+	if err != nil {
+		return nil, err
+	}
+	// Launch an EC2 instance with the resourcePrefix
+	sshKey, err := SetupSSHKey(ctx, resourcePrefix+"keypair", sshKeyPath)
+	if err != nil {
+		return nil, err
+	}
+
+	inst, err := ec2.NewInstance(ctx, resourcePrefix+name, &ec2.InstanceArgs{
+		InstanceType:             pulumi.String(instanceType),
+		Ami:                      pulumi.String(amiID),
+		SubnetId:                 subnetID,
+		AssociatePublicIpAddress: pulumi.Bool(public),
+		DisableApiTermination:    pulumi.Bool(config.RequireBool("debug")),
+		KeyName:                  sshKey.KeyName,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	err = AttachSecurityGroups(ctx, config, name, inst, securityGroups)
 
 	return inst, nil
 }
